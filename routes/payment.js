d56d6ea98a46b57640d3e1dd9786e417fe755533
Pawzzle <pawzzle.spark@gmail.com>
2026-04-14 14:24:08 +0800

    fix: resolve ReferenceError in payment routes

--- routes/payment.js
+++ routes/payment.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { createCheckoutSession, createBookingCheckoutSession, handleWebhook, verifyPayment } = require('../controllers/paymentController');
+const { createCheckoutSession, createBookingCheckoutSession, createAdoptionCheckoutSession, handleWebhook, verifyPayment } = require('../controllers/paymentController');
 const { authenticate } = require('../middleware/auth');
 
 // Create PayMongo Checkout Session
